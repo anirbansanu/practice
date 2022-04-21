91b756c6a0056f31c16b788fe064abca08f5b493
anirbansanu <anirbanmukherjee602@gmail.com>
2022-04-21 17:54:01 +0530

    initial Commit

---
+++ diagonalDifference.py
@@ -0,0 +1,23 @@
+# For example, the square matrix arr is shown below:
+# 1 2 3
+# 4 5 6
+# 9 8 9
+# The left-to-right diagonal = 1+5+9 = 15. 
+# The right to left diagonal = 3+5+9 = 17. 
+# Their absolute difference is 15 - 17 = 2.
+
+def diagonalDifference(arr):
+    i = 0 
+    j = len(arr)
+    rtol = 0
+    ltor = 0
+    while i < j:
+        rtol = rtol + arr[i][i]
+        ltor = ltor + arr[i][j-i-1]
+        i+=1
+    print('Right TO Left : ',rtol)
+    print('Left TO Right : ',ltor)
+    return abs(rtol - ltor) 
+
+a = [[1,2,3],[4,5,6],[9,8,9]]
+print(diagonalDifference(a))
---
+++ lonelyinteger.py
@@ -0,0 +1,17 @@
+# input : arr = [1,2,3,4,4,3,2,5,1]
+# output : 5
+def lonelyinteger(arr):
+    d={}
+    for i in range(len(arr)):
+        try:
+            d[arr[i]] = d.get(arr[i]) + 1
+        except:
+            d[arr[i]] = 1
+    s=''
+    for i in d.keys():
+        if d[i] == 1:
+            s += str(i)
+    return s
+
+if __name__ == '__main__':
+    print(lonelyinteger([1,2,3,4,5,3,2,1]))
