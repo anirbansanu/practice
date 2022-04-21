ad9a83bfa75a9e00516fee0422c103b8e76bab87
anirbansanu <anirbanmukherjee602@gmail.com>
2022-04-21 18:01:33 +0530

    initial commit

--- diagonalDifference.py
+++ diagonalDifference.py
@@ -1,7 +1,7 @@
 # For example, the square matrix arr is shown below:
 # 1 2 3
 # 4 5 6
-# 9 8 9
+# 9 8 9 
 # The left-to-right diagonal = 1+5+9 = 15. 
 # The right to left diagonal = 3+5+9 = 17. 
 # Their absolute difference is 15 - 17 = 2.
